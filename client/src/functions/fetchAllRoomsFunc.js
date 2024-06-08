00ec06a1d0b1128d48ea56ae3659b27bda83da84
amaan <amaan.ali.ug22@nsut.ac.in>
2024-06-08 10:45:26 +0530

    commit

--- client/src/functions/fetchAllRoomsFunc.js
+++ client/src/functions/fetchAllRoomsFunc.js
@@ -1,6 +1,8 @@
 import api from '../axios/api.js'
+import axios from 'axios';
 export const fetchAllRoomsFunc = async () => {
-    const response = await api.get('/all');
+    // const response = await api.get('/all');
+    const response = await axios.get('http://localhost:8000/rooms/all');
   
     return response.data.room;
     
